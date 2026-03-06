3245a600f3382bcf52fab212e984e7f1a08b4955
Caio Rodrigo <caiorodrgo@gmail.com>
2026-03-06 04:28:57 -0300

    feat: add Update User Use Case

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,42 @@
+import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user.js'
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
+import { bcrypt } from 'bcrypt'
+
+export class updateUserUseCase {
+    async execute(userId, updateUserParams) {
+        //1 - Se o email estiver sendo alterado, verificar se ele já está em uso:
+
+        if (updateUserParams.email) {
+            const getUserByEmail = new PostgresGetUserByEmailRepository()
+
+            const emailAlreadyExist = await getUserByEmail.execute(
+                updateUserParams.email,
+            )
+
+            if (emailAlreadyExist) {
+                throw new Error('The email already exists')
+            }
+        }
+
+        const user = { ...updateUserParams }
+
+        //2 - Se for a senha, criptografa-la antes de jogar no BD
+
+        if (updateUserParams.password) {
+            const hashedPassword = await bcrypt.hash(
+                updateUserParams.password,
+                10,
+            )
+
+            user.password = hashedPassword
+        }
+
+        const postgresUpdateUserUseCase = new PostgresUpdateUserRepository()
+        const updateUser = await postgresUpdateUserUseCase.execute(
+            userId,
+            updateUserParams,
+        )
+
+        return updateUser
+    }
+}
